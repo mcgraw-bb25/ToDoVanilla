ee9040a7304d363ccdfee087d1e8208153f3d8b7
matt <matthew.mcgraw@gmail.com>
2017-05-21 18:50:44 -0400

    implement delete_todo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// these are the models
 var Todo = function (name) {
     this.name = name
 };
@@ -12,7 +13,8 @@ var TodoManager = function () {
 
 TodoManager.prototype.add_todo = function (Todo) {
     var legal_todo = true;
-    for (var index = 0; index < this.todos.length; index = index + 1) {
+
+    for (var index = 0; index < this.length(); index = index + 1) {
         if (this.todos[index].name === Todo.name) {
             legal_todo = false;
         }
@@ -21,8 +23,8 @@ TodoManager.prototype.add_todo = function (Todo) {
         this.todos.push(Todo);
     }
     else {
-        console.log("Todo already exists!");
-        alert("" + Todo.name + " already exists!");
+        console.log("Cannot add, already exists!");
+        alert("" + Todo.name + " already exists, cannot add!");
     }
 };
 
@@ -30,6 +32,28 @@ TodoManager.prototype.length = function () {
     return this.todos.length;
 };
 
+TodoManager.prototype.delete_todo = function(TodoName) {
+    var legal_todo = false;
+    var position_to_delete = -1;
+
+    for (var index = 0; index < this.length(); index = index + 1) {
+        if (this.todos[index].name === TodoName) {
+            legal_todo = true;
+            position_to_delete = index;
+        }
+    };
+
+    if (legal_todo === true) {
+        this.todos.splice(position_to_delete, 1);
+    }
+    else {
+        console.log("Todo does not exist, cannot delete!");
+        alert("" + TodoName + " does not exist, cannot delete!");
+    }
+}
+
+
+// these are the controllers
 var todo_manager = new TodoManager();
 
 var add_todo = function add_todo (todo_name) {
@@ -38,6 +62,12 @@ var add_todo = function add_todo (todo_name) {
     update_view();
 };
 
+var delete_todo = function delete_todo (todo_name) {
+    todo_manager.delete_todo(todo_name);
+    update_view();
+};
+
+// this is the view
 var update_view = function () {
     var todo_list = document.getElementById("todo_list");
     todo_list.innerHTML = "";
@@ -61,8 +91,8 @@ var update_view = function () {
         todo_items = todo_items + " \
             <tr>\
                 <td>" + todo_manager.todos[index].name + "</td> \
-                <td>Complete</td> \
-                <td><button>Delete</button></td> \
+                <td><button>Complete</button></td> \
+                <td><button onclick=\"delete_todo('" + todo_manager.todos[index].name + "');\">Delete</button></td> \
             </tr> \
         ";       
     };
