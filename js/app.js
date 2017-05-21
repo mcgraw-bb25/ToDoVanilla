4f4356137e0b78c78a025f65230be2d3565558b6
matt <matthew.mcgraw@gmail.com>
2017-05-21 14:43:11 -0400

    todo functionality

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ Todo.prototype.get_name = function () {
     return this.name
 };
 
-var todo_list = [];
+var todo_manager = [];
 
 var add_todo = function add_todo (todo_name) {
     var new_todo = new Todo(todo_name);
@@ -16,5 +16,25 @@ var add_todo = function add_todo (todo_name) {
 
 var update_view = function () {
     var todo_list = document.getElementById("todo_list");
-    todo_list.innerHTML = "";
+    var header = "\
+    <table style=\"width:50%\"> \
+        <th>To Do</th> \
+        <th></th> \
+        <th></th> \
+    ";
+    var footer = " \
+        </table> \
+    ";
+    
+    var todo_items = "";
+    for (var index = 0; index < todo_list.length; index = index + 1) {
+        todo_items = todo_items + " \
+            <tr>\
+                <td>"+todo_manager[index].name+"</td> \
+                <td>Complete</td> \
+                <td>Delete</td> \
+            </tr> \
+        ";       
+    };
+    todo_list.innerHTML = header + todo_items + footer;
 };
