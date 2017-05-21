3dba6de42e4e8286e9810b114f0ae0573c15872d
matt <matthew.mcgraw@gmail.com>
2017-05-21 14:51:39 -0400

    adding table template

--- js/app.js
+++ js/app.js
@@ -10,24 +10,30 @@ var todo_manager = [];
 
 var add_todo = function add_todo (todo_name) {
     var new_todo = new Todo(todo_name);
-    todo_list.push(new_todo);
+    todo_manager.push(new_todo);
     update_view();
 };
 
 var update_view = function () {
     var todo_list = document.getElementById("todo_list");
+    todo_list.innerHTML = "";
+
     var header = "\
     <table style=\"width:50%\"> \
+        <tr> \
         <th>To Do</th> \
         <th></th> \
         <th></th> \
+        </tr> \
     ";
+
     var footer = " \
         </table> \
     ";
     
     var todo_items = "";
-    for (var index = 0; index < todo_list.length; index = index + 1) {
+
+    for (var index = 0; index < todo_manager.length; index = index + 1) {
         todo_items = todo_items + " \
             <tr>\
                 <td>"+todo_manager[index].name+"</td> \
@@ -36,5 +42,8 @@ var update_view = function () {
             </tr> \
         ";       
     };
-    todo_list.innerHTML = header + todo_items + footer;
+
+    var new_list = header + todo_items + footer;
+    console.log(new_list);
+    todo_list.innerHTML = new_list;
 };
