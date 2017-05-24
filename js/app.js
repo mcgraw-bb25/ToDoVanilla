3f001b73d5209d09067adb697288e00163282ec0
matt <matthew.mcgraw@gmail.com>
2017-05-23 20:42:04 -0400

    removing second manager, completed as attribute on Todo

--- js/app.js
+++ js/app.js
@@ -1,25 +1,42 @@
 // Models
 // Todo
 var Todo = function (name) {
-    this.name = name
+    this.name = name;
+    this.completed = false;
 };
 
 Todo.prototype.get_name = function () {
-    return this.name
+    return this.name;
 };
 
+Todo.prototype.get_status = function () {
+    return this.completed;
+};
+
+Todo.prototype.complete = function (name) {
+    this.completed = true;
+};
 
 
 // TodoManager
 var TodoManager = function () {
     this.todos = [];
+    this.completed = [];
+};
+
+TodoManager.prototype.todo_length = function () {
+    return this.todos.length;
+};
+
+TodoManager.prototype.completed_length = function () {
+    return this.completed.length;
 };
 
 TodoManager.prototype.add_todo = function (Todo) {
     var legal_todo = true;
 
-    for (var index = 0; index < this.length(); index = index + 1) {
-        if (this.todos[index].name === Todo.name) {
+    for (var index = 0; index < this.todo_length(); index = index + 1) {
+        if (this.todos[index].get_name() === Todo.get_name()) {
             legal_todo = false;
         }
     };
@@ -32,16 +49,12 @@ TodoManager.prototype.add_todo = function (Todo) {
     }
 };
 
-TodoManager.prototype.length = function () {
-    return this.todos.length;
-};
-
 TodoManager.prototype.delete_todo = function(TodoName) {
     var legal_todo = false;
     var position_to_delete = -1;
 
-    for (var index = 0; index < this.length(); index = index + 1) {
-        if (this.todos[index].name === TodoName) {
+    for (var index = 0; index < this.todo_length(); index = index + 1) {
+        if (this.todos[index].get_name() === TodoName) {
             legal_todo = true;
             position_to_delete = index;
         }
@@ -54,14 +67,36 @@ TodoManager.prototype.delete_todo = function(TodoName) {
         console.log("Todo does not exist, cannot delete!");
         alert("" + TodoName + " does not exist, cannot delete!");
     }
-}
+};
 
+
+TodoManager.prototype.complete_todo = function(TodoName) {
+    
+    var legal_todo = false;
+    var position_to_complete = -1;
+
+    for (var index = 0; index < this.todo_length(); index = index + 1) {
+        if (this.todos[index].get_name() === TodoName) {
+            legal_todo = true;
+            position_to_complete = index;
+        };
+    };    
+    
+    if (legal_todo === true) {
+        this.todos[position_to_complete].complete();
+        this.completed.push(this.todos[position_to_complete]);
+        this.todos.splice(position_to_complete, 1);
+    }
+    else {
+        console.log("Todo does not exist, cannot complete!");
+        alert("" + TodoName + " does not exist, cannot complete!");
+    }
+};
 
 
 
 // Controllers
 var todo_manager = new TodoManager();
-var completed_manager = new TodoManager();
 
 var add_todo = function add_todo (todo_name) {
     var new_todo = new Todo(todo_name);
@@ -75,12 +110,9 @@ var delete_todo = function delete_todo (todo_name) {
 };
 
 var complete_todo = function complete_todo (todo_name) {
-    var new_todo = new Todo(todo_name);
-    completed_manager.add_todo(new_todo);
-    todo_manager.delete_todo(todo_name);
+    todo_manager.complete_todo(todo_name);
     update_view();
 };
-
 
 
 // View
@@ -103,22 +135,22 @@ var update_view = function () {
     
     var todo_items = "";
 
-    for (var index = 0; index < todo_manager.length(); index = index + 1) {
+    for (var index = 0; index < todo_manager.todo_length(); index = index + 1) {
         todo_items = todo_items + " \
             <tr>\
-                <td>" + todo_manager.todos[index].name + "</td> \
-                <td><button onclick=\"complete_todo('" + todo_manager.todos[index].name + "');\">Complete</button></td> \
-                <td><button onclick=\"delete_todo('" + todo_manager.todos[index].name + "');\">Delete</button></td> \
+                <td>" + todo_manager.todos[index].get_name() + "</td> \
+                <td><button onclick=\"complete_todo('" + todo_manager.todos[index].get_name() + "');\">Complete</button></td> \
+                <td><button onclick=\"delete_todo('" + todo_manager.todos[index].get_name() + "');\">Delete</button></td> \
             </tr> \
         ";       
     };
 
     var completed_items = "";
 
-    for (var index = 0; index < completed_manager.length(); index = index + 1) {
+    for (var index = 0; index < todo_manager.completed_length(); index = index + 1) {                
         completed_items = completed_items + " \
             <tr bgcolor=\"#33cc33\">\
-                <td>" + completed_manager.todos[index].name + "</td> \
+                <td>" + todo_manager.completed[index].get_name() + "</td> \
                 <td>Complete</td> \
                 <td></td> \
             </tr> \
